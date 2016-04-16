b784e7db6ac564dfcd77522c3156a30f3758a75f
Fabian Schaper <fabian@Fabians-MacBook.local>
2016-04-16 16:34:06 +0200

    WATT WATT

--- frontend/views/home/homeController.js
+++ frontend/views/home/homeController.js
@@ -1,5 +1,9 @@
-app.controller('homeController', function($scope) {
+app.controller('homeController', function($scope, $rootScope) {
+        $rootScope.header = true;
         console.log("HALLO WELT");
 		// create a message to display in our view
 		$scope.message = 'home';
+        $scope.list = {
+            "name" : ""
+        };
 });
