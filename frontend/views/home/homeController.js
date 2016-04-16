af66e16ef66f447ba6b4fe586d64119453a7856d
Tobias Feller <tobiasmichaelfeller@gmail.com>
2016-04-16 14:43:52 +0200

    Add landing page design stuff thingy

--- frontend/views/home/homeController.js
+++ frontend/views/home/homeController.js
@@ -1,4 +1,5 @@
-app.controller('homeController', function($scope) {
+app.controller('homeController', function($scope, $rootScope) {
+        $rootScope.header = false;
         console.log("HALLO WELT");
 		// create a message to display in our view
 		$scope.message = 'home';
